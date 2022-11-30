17c188f998512d4e514283d45538d7a47db23603
Lauren Pyfer <laurenpyfer@Laurens-MacBook-Air-54.local>
2022-11-30 17:52:23 -0500

    done

--- discussion_12.py
+++ discussion_12.py
@@ -1,9 +1,11 @@
+#Lauren Pyfer
 import unittest
 import sqlite3
 import json
 import os
 import matplotlib.pyplot as plt
 # starter code
+
 
 # Create Database
 def setUpDatabase(db_name):
@@ -16,7 +18,8 @@ def setUpDatabase(db_name):
 # TASK 1
 # CREATE TABLE FOR EMPLOYEE INFORMATION IN DATABASE AND ADD INFORMATION
 def create_employee_table(cur, conn):
-    pass
+    cur.execute("CREATE TABLE IF NOT EXISTS Employees (employee_id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, hire_date TEXT, job_id INT, salary INT)")
+    conn.commit()
 
 # ADD EMPLOYEE'S INFORMTION TO THE TABLE
 
@@ -27,20 +30,62 @@ def add_employee(filename, cur, conn):
     file_data = f.read()
     f.close()
     # THE REST IS UP TO YOU
-    pass
+    js_data = json.loads(file_data)
+    for item in js_data:
+        employee_id = int(item('employee_id'))
+        first_name = item('first_name')
+        last_name = item('last_name')
+        hire_date = item('hire_date')
+        job_id = int(item('job_item'))
+        salary = int(item('salary'))
+        cur.execute("INSERT OR IGNORE INTO Employees (employee_id, first_name, last_name, hire_date, job_id, salary) VALUES (?, ?, ?, ?, ?, ?)"(employee_id, first_name, last_name, hire_date, job_id, salary))
 
 # TASK 2: GET JOB AND HIRE_DATE INFORMATION
 def job_and_hire_date(cur, conn):
-    pass
+    cur.execute("SELECT Employees.hire_date, Jobs.job_title FROM Employees JOIN Jobs ON Employees.job_id = Jobs.job_id ORDER BY Employees.hire_date")
+    return cur.fetchone()[1]
+   
+    #job_date_list = cur.fetchall()
+    #sorted_job_date_list = sorted(job_date_list, key=lambda x: x[0])
+    #return sorted_job_date_list[0][1]
 
 # TASK 3: IDENTIFY PROBLEMATIC SALARY DATA
 # Apply JOIN clause to match individual employees
 def problematic_salary(cur, conn):
-    pass
+    cur.execute("SELECT Employees.first_name Employees.last_name FROM Employees JOIN Jobs ON Employees.job_id = Jobs.job_id WHERE Employees.salary > Jobs.max_salary OR Employees.salary < Jobs.min_salary")
+    return cur.fetchall()
 
 # TASK 4: VISUALIZATION
 def visualization_salary_data(cur, conn):
-    pass
+    cur.execute("SELECT Employees.salary, Jobs.job_title FROM Employees JOIN Jobs ON Employees.job_id = Jobs.job_id")
+    salary_job_list = cur.fetchall()
+
+    job_list = []
+    salary_list = []
+    for item in salary_job_list:
+        job_list.append(item[1])
+        salary_list.append(item[0])
+
+    plt.figure()
+    plt.scatter(x = job_list, y = salary_list)
+
+    cur.execute("SELECT min_salary, max_salary, job_title FROM Jobs")
+    salary_job_list = cur.fetchall()
+    job_list = []
+    salary_list = []
+    for item in salary_job_list:
+        job_list.append(salary_job_list[2])
+        salary_list.append(salary_job_list[0])
+
+        job_list.append(salary_job_list[2])
+        salary_list.append(salary_job_list[1])
+
+    plt.scatter(x = job_list, y = salary_list, color = 'red')
+
+    plt.xticks(rotation = 30)
+    plt.tight_layout()
+
+    plt.show()
 
 class TestDiscussion12(unittest.TestCase):
     def setUp(self) -> None:
